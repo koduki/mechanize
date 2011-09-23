fa67152eddeba4b7b7a768e143b46751b542a99f
koduki <pascalm3@softbank126101144038.bbtec.net>
2011-09-24 00:42:47 +0900

    change log level.

--- src/main/scala/Mechanize.scala
+++ src/main/scala/Mechanize.scala
@@ -4,9 +4,16 @@ import com.gargoylesoftware.htmlunit.BrowserVersion
 import com.gargoylesoftware.htmlunit.WebClient
 import java.net.URL
 
+import org.apache.commons.logging.LogFactory 
+import org.apache.commons.logging.impl.Jdk14Logger
+import java.util.logging.Logger
+import java.util.logging.Level
+
 // vim: set ts=4 sw=4 et:
 class Mechanize {
     val client = new WebClient(BrowserVersion.FIREFOX_3)
+    val logger = LogFactory.getLog("com.gargoylesoftware.htmlunit").asInstanceOf[Jdk14Logger].getLogger
+    logger.setLevel(Level.OFF)
 
     def get(url:String):HtmlPage = {
         new HtmlPage(client.getPage(new URL(url)))
